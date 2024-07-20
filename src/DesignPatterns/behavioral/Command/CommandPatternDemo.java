5626ddc4717f34ec0d7f9197b971af2bb990bee2
Klodjan Mata <klodjanmata0@gmail.com>
2024-07-20 11:20:00 +0200

    small fix

--- src/DesignPatterns/behavioral/Command/CommandPatternDemo.java
+++ src/DesignPatterns/behavioral/Command/CommandPatternDemo.java
@@ -30,10 +30,3 @@ public class CommandPatternDemo {
         remote.pressButton();
     }
 }
-
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/klodjanmata/DesignPatterns.git
-git push -u origin main
